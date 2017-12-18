523fa97a1695516c66230af61ca15b6ae062da4e
Toolssss <egor.tveritinov@gmail.com>
2017-12-18 17:39:01 +0300

    SeekBar had plagued

--- app/src/main/java/com/example/toolss/filestest/MainActivity.java
+++ app/src/main/java/com/example/toolss/filestest/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.toolss.filestest;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import java.io.File;
@@ -15,15 +17,41 @@ import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText text;
+    private SeekBar seekBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (EditText)(findViewById(R.id.editText));
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                text.setTextSize(i);
+                SharedPreferences.Editor editor = getSharedPreferences("Settings",MODE_PRIVATE).edit();
+                editor.putInt("fontSize",i);
+                editor.commit();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
         try {
             InputStream is = this.openFileInput("abc.txt");
             Scanner scanner = new Scanner(is);
 
-            EditText text = (EditText)(findViewById(R.id.editText));
+
 
             while(scanner.hasNext()){
                 String s = scanner.nextLine();
@@ -46,7 +74,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         try{
-            EditText text = (EditText)(findViewById(R.id.editText));
             OutputStream os = this.openFileOutput("abc.txt",MODE_PRIVATE);
             PrintStream out = new PrintStream(os);
             out.println(text.getText().toString());
@@ -55,5 +82,14 @@ public class MainActivity extends AppCompatActivity {
 
         }
         catch (FileNotFoundException e){}
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences preferences = getSharedPreferences("Settings",MODE_PRIVATE);
+        int fontSize = preferences.getInt("fontSize",50);
+        text.setTextSize(fontSize);
+        seekBar.setProgress(fontSize);
     }
 }
